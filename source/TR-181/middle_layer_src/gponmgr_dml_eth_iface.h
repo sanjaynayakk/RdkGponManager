lgirdk/rdk-gponmanager

RDKBDEV-942 RdkGponManager: Modify as per new WAN Manager Design
Reason for change:
GPON manager should send link events received from GPON Json HAL to WAN manager as per new DM's defined
Test Procedure: Updated in JIRA
Risks: None
Signed-off-by: Sanchar Banerjee <sanchar.banerjee@external.telekom.de>
Change-Id: I8446dac6bed990d19da637e061c70cc7db0a2316

source/TR-181/middle_layer_src/gponmgr_dml_eth_iface.h
@@ -47,26 +47,20 @@
 //WAN Agent
 #define WAN_MGR_DBUS_PATH                     "/com/cisco/spvtg/ccsp/wanmanager"
 #define WAN_MGR_COMPONENT_NAME                "eRT.com.cisco.spvtg.ccsp.wanmanager"
-#define WAN_MGR_GPON_CPE_NAME                 "Device.X_RDK_WanManager.CPEInterface.3.Name"
+#define WAN_NOE_PARAM_NAME                    "Device.X_RDK_WanManager.CPEInterfaceNumberOfEntries"
+#define WAN_IF_NAME_PARAM_NAME                "Device.X_RDK_WanManager.CPEInterface.%d.Name"
+#define WAN_LINK_STATUS_PARAM_NAME            "Device.X_RDK_WanManager.CPEInterface.%d.LinkStatus"
+#define WAN_BASE_INTERFACE_PARAM_NAME         "Device.X_RDK_WanManager.CPEInterface.%d.BaseInterface"
 
-//ETH Manager
-#define ETH_MGR_COMPONENT_NAME                "eRT.com.cisco.spvtg.ccsp.ethagent"
-#define ETH_MGR_DBUS_PATH                     "/com/cisco/spvtg/ccsp/ethagent"
-
-#define ETH_MGR_IFACE_TABLE_NAME              "Device.Ethernet.X_RDK_Interface."
-#define ETH_MGR_IFACE                         "Device.Ethernet.X_RDK_Interface.%d."
-#define ETH_MGR_NO_OF_IFACE                   "Device.Ethernet.X_RDK_InterfaceNumberOfEntries"
-#define ETH_MGR_IFACE_NAME                    ETH_MGR_IFACE"Name"
-#define ETH_MGR_IFACE_LOWERLAYERS             ETH_MGR_IFACE"LowerLayers"
-#define ETH_MGR_IFACE_ENABLE                  ETH_MGR_IFACE"Enable"
-
+#define INTERFACE_UP "Up"
+#define INTERFACE_DOWN "Down"
 
 extern ANSC_HANDLE bus_handle;
 
-ANSC_STATUS Gponmgr_eth_addInterface( int iVeipIndex, char *LowerLayers, int *iVeipInstance);
-
-ANSC_STATUS Gponmgr_eth_setEnableInterface(int iVeipInstance, BOOL bflag);
-
 ANSC_STATUS Gponmgr_eth_setParams( char *pComponent, char *pBus, char *pParamName, char *pParamVal, enum dataType_e type, unsigned int bCommitFlag);
+
+ANSC_STATUS CosaDmlGponSetPhyStatusForWanManager(int iVeipIndex ,char *LowerLayers,char *PhyStatus);
+
+ANSC_STATUS CosaDmlGetLowerLayersInstanceInWanManager(char *pLowerLayers, INT *piInstanceNumber);
 
 #endif

sanjaynayakk/RdkGponManager

(cherry picked from commit c394f9237e4eea4513a24ea4a316b2f775e18121)

source/TR-181/middle_layer_src/gponmgr_dml_eth_iface.h
@@ -44,9 +44,21 @@
 #include "gponmgr_dml_backendmgr.h"
 #include "gponmgr_dml_plugin_main.h"
 
-//WAN Agent
+//WAN Manager
 #define WAN_MGR_DBUS_PATH                     "/com/cisco/spvtg/ccsp/wanmanager"
 #define WAN_MGR_COMPONENT_NAME                "eRT.com.cisco.spvtg.ccsp.wanmanager"
+#if defined(WAN_MANAGER_UNIFICATION_ENABLED)
+#define INTERFACE_UP "Up"
+#define INTERFACE_DOWN "Down"
+#define WAN_NOE_PARAM_NAME                    "Device.X_RDK_WanManager.InterfaceNumberOfEntries"
+#define WAN_IF_NAME_PARAM_NAME                "Device.X_RDK_WanManager.Interface.%d.Name"
+#define WAN_LINK_STATUS_PARAM_NAME            "Device.X_RDK_WanManager.Interface.%d.BaseInterfaceStatus"
+#define WAN_BASE_INTERFACE_PARAM_NAME         "Device.X_RDK_WanManager.Interface.%d.BaseInterface"
+#else
+#define WAN_NOE_PARAM_NAME                    "Device.X_RDK_WanManager.CPEInterfaceNumberOfEntries"
+#define WAN_IF_NAME_PARAM_NAME                "Device.X_RDK_WanManager.CPEInterface.%d.Name"
+#define WAN_LINK_STATUS_PARAM_NAME            "Device.X_RDK_WanManager.CPEInterface.%d.Wan.LinkStatus"
+#define WAN_BASE_INTERFACE_PARAM_NAME         "Device.X_RDK_WanManager.CPEInterface.%d.Phy.Path"
 #define WAN_MGR_GPON_CPE_NAME                 "Device.X_RDK_WanManager.CPEInterface.3.Name"
 
 //ETH Manager
@@ -60,13 +72,24 @@
 #define ETH_MGR_IFACE_LOWERLAYERS             ETH_MGR_IFACE"LowerLayers"
 #define ETH_MGR_IFACE_ENABLE                  ETH_MGR_IFACE"Enable"
 
+#endif
+
 
 extern ANSC_HANDLE bus_handle;
 
+#if defined(WAN_MANAGER_UNIFICATION_ENABLED)
+ANSC_STATUS Gponmgr_eth_setParams( char *pComponent, char *pBus, char *pParamName, char *pParamVal, enum dataType_e type, unsigned int bCommitFlag);
+
+ANSC_STATUS CosaDmlGponSetPhyStatusForWanManager(int iVeipIndex ,char *LowerLayers,char *PhyStatus);
+
+ANSC_STATUS CosaDmlGetLowerLayersInstanceInWanManager(char *pLowerLayers, INT *piInstanceNumber);
+#else
 ANSC_STATUS Gponmgr_eth_addInterface( int iVeipIndex, char *LowerLayers, int *iVeipInstance);
 
 ANSC_STATUS Gponmgr_eth_setEnableInterface(int iVeipInstance, BOOL bflag);
 
 ANSC_STATUS Gponmgr_eth_setParams( char *pComponent, char *pBus, char *pParamName, char *pParamVal, enum dataType_e type, unsigned int bCommitFlag);
+
+#endif
 
 #endif